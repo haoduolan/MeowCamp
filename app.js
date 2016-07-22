86f58745158baf44efee6347a2504b4e719e7202
haoduolan <lanyang@email.arizona.edu>
2016-07-22 20:04:15 +0000

    modified app.js

--- app.js
+++ app.js
@@ -16,8 +16,9 @@ var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes      = require("./routes/index")
 
-// mongoose.connect("mongodb://localhost/yelp_camp");
-mongoose.connect("haoduolan:yanglan1991@ds027215.mlab.com:27215/yelpcamp");
+
+ mongoose.connect(process.env.DATABASEURL);
+//mongoose.connect("haoduolan:yanglan1991@ds027215.mlab.com:27215/yelpcamp");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
